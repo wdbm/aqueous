81b693547654ca8c4fcca6860d9e67cb6ae8aa35
Will Breaden Madden <zicodian@gmail.com>
2016-05-31 17:16:22 +0200

    add check_spelling.sh, change word_count_estimate.sh to estimate_word_count.sh

---
+++ check_spelling.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# -t:           mode: TeX
+# --lang=en_GB: English G.B. dictionary
+# -c:           check single file
+
+aspell -t --lang=en_GB -c abstract.tex
+aspell -t --lang=en_GB -c acknowledgements.tex
+aspell -t --lang=en_GB -c abstract.tex
+aspell -t --lang=en_GB -c acknowledgements.tex
+aspell -t --lang=en_GB -c introduction.tex
+aspell -t --lang=en_GB -c chapter_1.tex
+aspell -t --lang=en_GB -c future.tex
