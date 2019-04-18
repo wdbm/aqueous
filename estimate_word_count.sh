2e3886c399e7d2abe12b2a842a7aa28fd1a18008
Will Breaden Madden <wbm@protonmail.ch>
2019-04-18 16:58:19 +0100

    update

--- estimate_word_count.sh
+++ estimate_word_count.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-word_count_estimate=$(echo \
-$(wc -w abstract.tex | cut -d' ' -f1) + \
-$(wc -w acknowledgements.tex | cut -d' ' -f1) + \
-$(wc -w introduction.tex | cut -d' ' -f1) + \
-$(wc -w chapter_1.tex | cut -d' ' -f1) + \
-$(wc -w future.tex | cut -d' ' -f1) \
+word_count_estimate=$(echo                       \
+$(wc -w abstract.tex          | cut -d' ' -f1) + \
+$(wc -w acknowledgements.tex  | cut -d' ' -f1) + \
+$(wc -w introduction.tex      | cut -d' ' -f1) + \
+$(wc -w chapter_1.tex         | cut -d' ' -f1) + \
+$(wc -w future.tex            | cut -d' ' -f1)   \
 | bc)
 echo "word count estimate: "${word_count_estimate}""
